b0ab052d4f82aa8a95a0e1631e7e46d9bb0ddaef
BorisSolomonia <78170135+BorisSolomonia@users.noreply.github.com>
2022-09-18 16:44:47 +0400

    Update DigitsSumCalculator.java

--- src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
+++ src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
@@ -2,5 +2,13 @@ package lang.print.gaps.finalModuleTask;
 
 public class DigitsSumCalculator {
     public void calculateSum(int number) {
+        System.out.println(sumNum(number));
+    }
+    public int sumNum(int num){
+        int k = num%10;
+        if (num/10 < 1){
+            return k;
+        }
+        return k+sumNum(num/10);
     }
 }
