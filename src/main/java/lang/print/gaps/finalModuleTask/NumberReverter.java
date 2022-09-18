24a876e0e79a2b2abc8cf298618f11f3debd8ae5
BorisSolomonia <78170135+BorisSolomonia@users.noreply.github.com>
2022-09-18 16:27:40 +0400

    Update NumberReverter.java

--- src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
+++ src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
@@ -2,5 +2,14 @@ package lang.print.gaps.finalModuleTask;
 
 public class NumberReverter {
     public void revert(int number) {
+        System.out.println(reverseNumber(number);
+    }
+    public static int reverseNumber(int reverse){
+        int k = reverse%10;
+        int g = (int) String.valueOf(reverse).chars().count();
+        if (reverse/10 < 1){
+            return k;
+        }
+        return (int) (k*Math.pow(10,g-1)+reverseNumber(reverse/10));
     }
 }
